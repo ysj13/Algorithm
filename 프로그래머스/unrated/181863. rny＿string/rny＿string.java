6e9d65ae881f9d57bbe202f0448be3681f364dc5
js <59955248+ysj13@users.noreply.github.com>
2023-06-14 23:27:38 +0900

    [unrated] Title: rny_string, Time: 1.41 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        
+		for(char c : rny_string.toCharArray()) {
+			if(c == 'm') {
+				answer += "rn";
+			} else {
+				answer += Character.toString(c);
+			}
+		}
+        
+        return answer;
+    }
+}
